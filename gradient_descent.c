09cb6daa0c46cafe8961222953a076e4c6a415dc
temmiejoestar <temmiechanowo@gmail.com>
2026-02-18 19:45:11 +0100

    Updated "calculeate_error" so it can take as input a matrix  if AxB

--- gradient_descent.c
+++ gradient_descent.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "matrix.h"
 
@@ -19,11 +20,20 @@ Matrix backward_pass(Matrix Prediction, Matrix Input, Matrix Target) {
 }
 
 float calculate_error(Matrix Prediction, Matrix Target) {
-    float pred = get_value(Prediction, 0, 0);
-    float targ = get_value(Target, 0, 0);
-    
-    float diff = pred - targ;
-    return diff * diff;
+    if (Prediction.rows != Target.rows || Prediction.cols != Target.cols) {
+        fprintf(stderr,RED_TEXT("Error: Matrices must be the same size to calculate error !\n"));
+        exit(1);
+    }
+
+    int total = Prediction.rows * Prediction.cols;
+    float sum = 0.0f;
+    for (int i = 0; i < total; i++){
+            float pred = Prediction.data[i];
+            float targ = Target.data[i];
+            float diff = (pred - targ) * (pred - targ);
+            sum += diff;
+    }
+    return (sum/total);
 }
 
 Matrix update_weights(Matrix Weights, Matrix Gradient, float learning_rate) {
@@ -45,7 +55,7 @@ void train(Matrix Input, Matrix Weights, Matrix Target, int steps, float learnin
         Matrix Gradient = backward_pass(Prediction, Input, Target);
         Matrix New_weights = update_weights(current, Gradient, learning_rate);
         
-        printf("\nStep %d: Error = %.4f", i+1, error);
+        printf("\nStep %d: Error = %.4f\n", i+1, error);
         
         free_matrix(Prediction);
         free_matrix(Gradient);
@@ -53,4 +63,29 @@ void train(Matrix Input, Matrix Weights, Matrix Target, int steps, float learnin
         current = New_weights;
     }
     free_matrix(current);
+}
+
+int main() {
+
+Matrix Input = create_matrix(1, 3);
+set_value(Input, 0, 0, 1.0f);
+set_value(Input, 0, 1, 2.0f);
+set_value(Input, 0, 2, 3.0f);
+
+
+Matrix Initial_weight = create_matrix(3, 1);
+set_value(Initial_weight, 0, 0, 0.1f);
+set_value(Initial_weight, 1, 0, 0.1f);
+set_value(Initial_weight, 2, 0, 0.1f);
+
+Matrix Target = create_matrix(1, 1);
+set_value(Target, 0, 0, 14.0f);
+    
+train(Input, Initial_weight, Target, 10, 0.01f);
+//Matrix Prediction = matrix_multiply(Input, Initial_weight);
+//calculate_error(Prediction,Target);
+    
+free_matrix(Input);
+free_matrix(Initial_weight);
+free_matrix(Target);
 }
