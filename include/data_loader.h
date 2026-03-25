ee98ab5c281fd8b136ad688165c48d2a9dc33956
temmiejoestar <temmiechanowo@gmail.com>
2026-03-25 12:28:09 +0100

    CSV control files

---
+++ include/data_loader.h
@@ -0,0 +1,7 @@
+#ifndef DATA_LOADER_H
+#define DATA_LOADER_H
+
+#include "matrix.h"
+
+
+#endif 
---
+++ src/data_loader.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+
+int main()
+{
+    printf(YELLOW_TEXT("WORK IN PROGRESS: Will contains function to read/load CSV files\n"));
+    return 0;
+}
+
